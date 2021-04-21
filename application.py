ea044e5b0852f5cf58332edfbbbb0c8af3877ddb
dungdev <dung.truong@kompa.ai>
2021-04-21 10:56:51 +0700

    update code

--- application.py
+++ application.py
@@ -1,12 +1,12 @@
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route('/')
+@application.route('/')
 def hello():
-    return "Hello World!!"
+    return "Xin Chao!!!!!"
 
 
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0')
+    application.run(host= '0.0.0.0', port=5000, debug=True)
